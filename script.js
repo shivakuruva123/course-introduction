5b0b7b95e3fa1d48c1450cf1bfc429e4ccf875e6
shivakuruva123 <shivakuruva123@gmail.com>
2024-05-27 10:45:17 +0530

    project add

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add JavaScript functionality here if needed
+document.getElementById('enroll-btn').addEventListener('click', function() {
+    alert('You are now enrolled in the Captivating Landing Page Course!');
+  });
+  
